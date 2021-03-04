d704ac4d3819442557e432dfae699a4d33af8c6c
Bytewright <bytewright@t-online.de>
2021-03-04 18:40:02 +0100

    init commit2

---
+++ src/main/java/org/bytewright/MediaLibManager/MediaLibManagerApplication.java
@@ -0,0 +1,13 @@
+package org.bytewright.MediaLibManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaLibManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaLibManagerApplication.class, args);
+	}
+
+}
